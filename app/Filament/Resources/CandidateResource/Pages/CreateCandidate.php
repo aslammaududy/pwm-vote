b52339cb2e776bc820d0f4b9f0d4332814f67684
Aslam Maududy <aslammaududy@outlook.com>
2023-12-03 17:53:38 +0700

    form pendaftaran kandidat

--- app/Filament/Resources/CandidateResource/Pages/CreateCandidate.php
+++ app/Filament/Resources/CandidateResource/Pages/CreateCandidate.php
@@ -3,11 +3,26 @@
 namespace App\Filament\Resources\CandidateResource\Pages;
 
 use App\Filament\Resources\CandidateResource;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateCandidate extends CreateRecord
 {
     protected static string $resource = CandidateResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 
     protected function getActions(): array
     {
