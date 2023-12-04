f65977ca5074e4882867568b7262add982ce99f7
Aslam Maududy <aslammaududy@outlook.com>
2023-12-04 22:37:26 +0700

    restrict access ke dash jika bukan admin

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -3,8 +3,17 @@
 namespace App\Filament\Pages;
 
 
+use App\Filament\Resources\CandidateResource;
+
 class Dashboard extends \Filament\Pages\Dashboard
 {
+    public function mount(): void
+    {
+        if (!auth()->user()->is_admin) {
+            redirect()->intended(CandidateResource::getUrl('vote'));
+        }
+    }
+
     public static function shouldRegisterNavigation(): bool
     {
         return auth()->user()->is_admin;
