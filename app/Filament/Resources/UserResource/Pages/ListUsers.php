861617974a2127a5977e9e7a33bc9aaac2a3bd62
Aslam Maududy <aslammaududy@outlook.com>
2023-12-08 20:41:34 +0700

    import user dari csv

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -2,11 +2,15 @@
 
 namespace App\Filament\Resources\UserResource\Pages;
 
+use App\Filament\Imports\UserImporter;
 use App\Filament\Resources\UserResource;
+use EightyNine\ExcelImport\ExcelImportAction;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Builder;
+use Konnco\FilamentImport\Actions\ImportAction;
+use Konnco\FilamentImport\Actions\ImportField;
 
 class ListUsers extends ListRecords
 {
@@ -15,8 +19,10 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            ExcelImportAction::make()
+                ->color("primary"),
             Actions\CreateAction::make()
-            ->label('Tambah Pengguna'),
+                ->label('Tambah Pengguna'),
         ];
     }
 
